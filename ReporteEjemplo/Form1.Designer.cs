4fe4c379b14a7bf420905475016cd992e0b21171
felixacdc <pfmata01@gmail.com>
2017-03-09 15:04:05 -0600

    [add] view first report

--- ReporteEjemplo/Form1.Designer.cs
+++ ReporteEjemplo/Form1.Designer.cs
@@ -29,24 +29,26 @@
         private void InitializeComponent()
         {
             this.crystalReportViewer1 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
+            this.Ejemplo1 = new ReporteEjemplo.Ejemplo();
             this.SuspendLayout();
             // 
             // crystalReportViewer1
             // 
-            this.crystalReportViewer1.ActiveViewIndex = -1;
+            this.crystalReportViewer1.ActiveViewIndex = 0;
             this.crystalReportViewer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.crystalReportViewer1.Cursor = System.Windows.Forms.Cursors.Default;
             this.crystalReportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.crystalReportViewer1.Location = new System.Drawing.Point(0, 0);
             this.crystalReportViewer1.Name = "crystalReportViewer1";
-            this.crystalReportViewer1.Size = new System.Drawing.Size(732, 405);
+            this.crystalReportViewer1.ReportSource = this.Ejemplo1;
+            this.crystalReportViewer1.Size = new System.Drawing.Size(1098, 539);
             this.crystalReportViewer1.TabIndex = 0;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(732, 405);
+            this.ClientSize = new System.Drawing.Size(1098, 539);
             this.Controls.Add(this.crystalReportViewer1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -58,6 +60,7 @@
         #endregion
 
         private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer1;
+        private Ejemplo Ejemplo1;
     }
 }
 
--- ReporteEjemplo/Form1.cs
+++ ReporteEjemplo/Form1.cs
@@ -6,11 +6,14 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data.Odbc;
 
 namespace ReporteEjemplo
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
@@ -18,7 +21,29 @@ namespace ReporteEjemplo
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            try
+            {
+                String query = "Select pro.NombreProducto,cat.NombreCategoria,pro.PrecioUnitario,pro.UnidadesEnExistencia" +
+                    " From Productos pro inner join Categorias2 cat" + " On pro.IdCategoria = cat.id;";
+                OdbcConnection cnn;
 
+                cnn = new OdbcConnection("Driver={SQL Server};Server=1CAE-GUA-PC152\\SQLEXPRESS;UID=;PWD=;Database=Neptuno;");
+                cnn.Open();
+
+                Ejemplo Reporte = new Ejemplo();
+                DsReporteEjemplo dsReportePCE = new DsReporteEjemplo();
+                OdbcDataAdapter adapter = new OdbcDataAdapter(query, cnn);
+                adapter.Fill(dsReportePCE, "Productos");
+                Reporte.SetDataSource(dsReportePCE);
+
+                crystalReportViewer1.ReportSource = Reporte;
+
+                MessageBox.Show("Conectado");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("No se conecto la db: " + ex.ToString());
+            }
         }
     }
 }
